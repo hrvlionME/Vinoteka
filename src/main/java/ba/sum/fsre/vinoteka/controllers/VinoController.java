c6923e9f3c813ccc831072cce4f2b1f055082925
ivana <mail@example.com>
2024-01-24 08:31:37 +0100

    homepage

--- src/main/java/ba/sum/fsre/vinoteka/controllers/VinoController.java
+++ src/main/java/ba/sum/fsre/vinoteka/controllers/VinoController.java
@@ -6,12 +6,17 @@ import ba.sum.fsre.vinoteka.repositories.UserRepository;
 import ba.sum.fsre.vinoteka.repositories.VinoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< HEAD
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+=======
+import org.springframework.security.access.prepost.PreAuthorize;
+>>>>>>> dc7c5b8 (homepage)
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -67,6 +72,14 @@ public class VinoController {
         model.addAttribute("vina", vina);
         return "showcase";
     }
+
+ /*   @PostMapping("/edit/delete/{}")
+     @PreAuthorize("hasAuthority('ADMIN')")
+    public String deleteVino(@PathVariable Long ) {
+        vinoRepo.deleteById();
+        return "redirect:/edit";
+    }
+ */
 
 
     @PostMapping("/upload")
